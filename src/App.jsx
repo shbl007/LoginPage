9a7235ab6ec93d012deef19246ba85ce95806741
shbl007 <shbl.salim@gmail.com>
2024-08-07 10:21:08 +0530

    heightscreenCorrection

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Register from "./components/Register";
 
 function App() {
   return (
-    <div className="flex justify-center gap-10">
+    <div className="flex justify-center gap-10 h-screen">
     <div className="p-5 sm:p-8 flex flex-col gap-6 justify-center ">
       <div className="flex flex-col gap-3 text-center">
         <h1 className="font-bold text-5xl">Welcome Back!</h1>
